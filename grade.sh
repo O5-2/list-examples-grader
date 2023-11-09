6de9df706d4a7e7195a6dd59072c0ffdd06f8785
O5-2 <35108232+O5-2@users.noreply.github.com>
2023-11-08 16:39:40 -0800

    Minor code cleanup

--- grade.sh
+++ grade.sh
@@ -1,12 +1,13 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
+# Current status: Regex isn't working, trying to fix that
+
 rm -rf student-submission
 rm -rf grading-area
 
 mkdir grading-area
 
 git clone $1 student-submission 2> grading-area/blank.txt
-#echo 'Finished cloning'
 
 if ! [[ -f student-submission/ListExamples.java ]]
 then
@@ -17,10 +18,8 @@ fi
 cp student-submission/ListExamples.java grading-area/ListExamples.java
 cp *.java grading-area/
 cp -r lib grading-area/lib
-# Is this right?
 
 javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar grading-area/*.java
-# Does this work?
 if [[ $? -ne 0 ]]
 then
   echo "Error during compilation"
@@ -28,7 +27,6 @@ then
 fi
 
 java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore grading-area/TestListExamples > grading-area/result.txt
-# Is this right?
 regexOne='/(\d+)/'
 regexTwo='/(\d+).*(\d+)/'
 
@@ -42,10 +40,8 @@ then
   echo "Perfect score"
   [[ $grepOne =~ $regexOne ]]
   echo "${BASH_REMATCH[1]} out of ${BASH_REMATCH[1]} tests passed"
-  # Done?
 else
   echo "Some tests failed"
   [[ $grepTwo =~ $regexTwo ]]
   echo "${BASH_REMATCH[1]} out of ${BASH_REMATCH[2]} tests passed"
-  # Done?
 fi
